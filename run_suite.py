a7675d0d856e2756db54209fbe23615151f2659a
feifei112 <59948316+feifei112@users.noreply.github.com>
2020-02-14 19:26:55 +0800

    02
    000

--- run_suite.py
+++ run_suite.py
@@ -4,5 +4,5 @@ from tools.HTMLTestRunner import HTMLTestRunner
 # 2. 组装测试套件
 suite = unittest.defaultTestLoader.discover("./scripts",pattern="test*.py")
 # 3. 获取报告存储文件流 实例化HTMLTestRunner 调用run执行测试套件
-with open("./report/ihrm_report.html", "wb") as f:
+with open("./report/report.html", "wb") as f:
     HTMLTestRunner(stream=f).run(suite)
